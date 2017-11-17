8b518b2b6f13539ae72f74960449dadb648fddb7
MoyTW <moytravis@gmail.com>
2017-11-16 21:25:15 -0800

    Trying out Orchestra

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [orchestra "2017.11.12-1"]]
   :main ^:skip-aot dcs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dcs/core.clj
+++ src/dcs/core.clj
@@ -1,7 +1,27 @@
 (ns dcs.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.spec.alpha :as s]
+            [orchestra.spec.test :as st]))
+
+(defonce last-id (atom 0))
+
+(defn- next-id []
+  (swap! last-id inc))
+
+(s/def ::id int?)
+(s/def ::name string?)
+
+(s/def ::devil (s/keys :req [::id ::name]))
+
+(s/fdef generate-devil
+  :ret ::devil)
+
+(defn- generate-devil []
+  {::id (next-id)
+   ::name (str (dec (next-id)))})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (st/instrument)
+  (clojure.pprint/pprint (generate-devil)))
