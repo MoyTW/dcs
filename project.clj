ee3624c2a424a6862b500aab465956f871845f8c
MoyTW <moytravis@gmail.com>
2017-11-19 17:34:51 -0800

    Begin defining the Devils
    I'll be using a lightweight EC system, Brute, though I'll probably end up
    wrapping it heavily.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [orchestra "2017.11.12-1"]]
+                 [orchestra "2017.11.12-1"]
+                 [brute "0.4.0"]]
   :main ^:skip-aot dcs.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dcs/core.clj
+++ src/dcs/core.clj
@@ -1,33 +1,72 @@
 (ns dcs.core
   (:gen-class)
-  (:require [clojure.spec.alpha :as s]
+  (:require [brute.entity :as e]
+            [clojure.spec.alpha :as s]
             [dcs.contract-template :as ct]
             [orchestra.spec.test :as st]))
 
-(defonce last-id (atom 0))
-
-(defn- next-id []
-  (swap! last-id inc))
-
-(s/def ::id int?)
-
 ;; *****************************************************************************
-;; * DEVILS *
+;; * INVENTORY COMPONENT *
 ;; *****************************************************************************
 
-(s/def ::name string?)
+(s/def ::max-size int?)
+(s/def ::contents (s/coll-of [::item-id]))
 
-(s/def ::devil (s/keys :req [::id ::name]))
+(s/def ::inventory (s/keys :req-un [::contents ::max-size]))
 
-(s/fdef generate-devil
-  :ret ::devil)
+(s/fdef ->Inventory
+  :args (s/cat :contents ::contents :max-size ::max-size)
+  :ret ::inventory)
 
-(defn- generate-devil []
-  {::id (next-id)
-   ::name (str (dec (next-id)))})
+(defrecord Inventory [contents max-size])
+
+;; *****************************************************************************
+;; * CAPACITY COMPONENT *
+;; *****************************************************************************
+
+(s/def ::contracts (s/coll-of [::item-id]))
+
+(s/def ::capacity (s/keys :req-un [::contracts]))
+
+(s/fdef ->Capacity
+  :args (s/cat :contracts ::contracts)
+  :ret ::capacity)
+
+(defrecord Capacity [contracts])
+
+;; *****************************************************************************
+;; * MAGIC COMPONENT *
+;; *****************************************************************************
+
+(s/def ::domain #{:wood :fire :earth :metal :water})
+(s/def ::potential #{0.5 0.75 1.0 1.5 2})
+(s/def ::xp int?)
+(s/def ::proficiency (s/keys :req-un [::domain ::xp ::aptitude]))
+
+(s/def ::proficiencies (s/coll-of ::proficiency))
+
+(s/fdef ->Magic
+  :args (s/cat :domains ::proficiencies))
+
+(defrecord Magic [proficiencies])
+
+(defn- generate-devil [system]
+  (let [devil (e/create-entity)]
+    (-> system
+        (e/add-entity devil)
+        (e/add-component devil (->Inventory [] 5))
+        (e/add-component devil (->Capacity []))
+        (e/add-component devil (->Magic [])))))
+
+(defn- seed-world [system]
+  (reduce (fn [system _] (generate-devil system))
+          (e/create-system)
+          (take 10 (repeat 1))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (st/instrument)
-  (clojure.pprint/pprint (generate-devil)))
+  (let [sys (seed-world (e/create-system))]
+    (clojure.pprint/pprint sys)))
+
+(st/instrument)
