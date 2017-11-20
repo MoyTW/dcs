0691704dae7c60ed9df2a0e7d38bfc45b189f3ae
MoyTW <moytravis@gmail.com>
2017-11-19 19:36:39 -0800

    Add random generation for Magic

--- src/dcs/components/magic.clj
+++ src/dcs/components/magic.clj
@@ -2,17 +2,58 @@
   (:require [brute.entity :as e]
             [clojure.spec.alpha :as s]))
 
-(s/def ::domain #{:wood :fire :earth :metal :water})
-(s/def ::potential #{0.5 0.75 1.0 1.5 2})
+(def domain #{:wood :fire :earth :metal :water})
+(def aptitude #{0.5 0.75 1.0 1.5 2})
+
+(s/def ::domain domain)
+(s/def ::aptitude aptitude)
 (s/def ::xp int?)
-(s/def ::proficiency (s/keys :req-un [::domain ::xp ::aptitude]))
+(s/def ::proficiency (s/keys :req [::domain ::xp ::aptitude]))
 
 (s/def ::proficiencies (s/coll-of ::proficiency))
 
 (s/fdef ->Magic
-  :args (s/cat :domains ::proficiencies))
+  :args (s/cat :proficiencies ::proficiencies))
 
 (defrecord Magic [proficiencies])
 
 (defn create [proficiencies]
   (->Magic proficiencies))
+
+;; Probably belongs somewhere else. Uses D&D levels. Formula is: XP-for-level =
+;; L*(L-1)*500
+(def level->xp-chart
+  {1 0
+   2 1000
+   3 3000
+   4 6000
+   5 10000
+   6 15000
+   7 21000
+   8 28000
+   9 36000
+   10 45000
+   11 55000
+   12 66000
+   13 78000
+   14 91000
+   15 105000
+   16 120000
+   17 136000
+   18 153000
+   19 171000
+   20 190000})
+
+;; The equation for XP-to-level is D&D's, n*(n-1)*500
+;;
+;; This inverts it to n = (1 + sqrt(1 - 4(x/500))) / 2 - we leave out the +- in
+;; favor of just + because we're not interested in the left side. I'm glad I
+;; remembered how to do this. Like it's not hard, but hey! that high school
+;; algebra not *totally* wasted!
+;;
+;; Probably belongs somewhere else.
+(defn xp->level [xp]
+  (int (/ (+ 1 (Math/sqrt (- 1 (* 4 (* -1 (/ xp 500)))))) 2)))
+
+(defn xp-for-level [level]
+  (level->xp-chart level))
--- src/dcs/core.clj
+++ src/dcs/core.clj
@@ -12,11 +12,45 @@
 ;; * MAGIC COMPONENT *
 ;; *****************************************************************************
 
+(defn create-rng [seed]
+  (java.util.Random. seed))
+
+(defn seeded-shuffle [^java.util.Random rng ^java.util.Collection coll]
+  (let [al (java.util.ArrayList. coll)]
+    (java.util.Collections/shuffle al rng)
+    (clojure.lang.RT/vector (.toArray al))))
+
+(defn seeded-next-int
+  ([^java.util.Random rng]
+   (.nextInt rng))
+  ([^java.util.Random rng bound]
+   (.nextInt rng bound)))
+
+(defn- create-proficiency [^java.util.Random rng max-level domain]
+  {::magic/domain domain
+   ::magic/aptitude (first (seeded-shuffle rng magic/aptitude))
+   ::magic/xp (seeded-next-int rng (magic/xp-for-level max-level))})
+
+(defn- create-rand-magic
+  [^java.util.Random rng max-domains max-level]
+  (->> (seeded-shuffle rng magic/domain)
+       (take (inc (seeded-next-int rng max-domains)))
+       (map (partial create-proficiency rng max-level))
+       magic/create))
+
+(defn- generate-summoner [system rng]
+  (let [summoner (e/create-entity)]
+    (-> system
+        (e/add-entity summoner)
+        (e/add-component summoner (inventory/create [] 10))
+        (e/add-component summoner (capacity/create []))
+        (e/add-component summoner (create-rand-magic rng 2 3)))))
+
 (defn- generate-devil [system]
   (let [devil (e/create-entity)]
     (-> system
         (e/add-entity devil)
-        (e/add-component devil (inventory/create [] 5))
+        (e/add-component devil (inventory/create [] 50))
         (e/add-component devil (capacity/create []))
         (e/add-component devil (magic/create [])))))
 
@@ -28,7 +62,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [sys (seed-world (e/create-system))]
+  (let [rng (java.util.Random. 1)
+        sys (seed-world (e/create-system))]
     (clojure.pprint/pprint sys)))
 
 (st/instrument)
